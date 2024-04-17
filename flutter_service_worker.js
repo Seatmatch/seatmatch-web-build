16c9f9d33ba4d904ca4c7f01739d96a31f9b23d3
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2024-04-17 16:38:37 +0800

    update prod build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4cbe8f127326e6dc62e43df67f6c3e4f",
-"index.html": "95dbc7085dce87c1f6d534ea2c4fd177",
-"/": "95dbc7085dce87c1f6d534ea2c4fd177",
-"main.dart.js": "0cd335d5141d612e61801d45d6b7c3fd",
+"index.html": "c2521616c705cda8bea7f39c6102f8d1",
+"/": "c2521616c705cda8bea7f39c6102f8d1",
+"main.dart.js": "edd88e34459676c0871d3c8f2dba69fe",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "e2a30daff2c76ca4995a920164506dcf",
 "icons/Icon-192.png": "28176879ef449ad5aafcfb02f1be6209",
